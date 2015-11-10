e2401d6d9f47545c311b5c7651658af4ad155493
mschnur <matthew.p.schnur@gmail.com>
2015-11-10 13:12:08 -0500

    Bigger arrays, more randomness

--- src/com/schnur/TestArraySort.java
+++ src/com/schnur/TestArraySort.java
@@ -1,12 +1,9 @@
 package com.schnur;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
@@ -18,66 +15,32 @@ import static org.junit.Assert.assertArrayEquals;
 public class TestArraySort
 {
    private static final int NUM_ARRAYS  = 150;
-   private static final int MAX_ARR_LEN = 1000;
-   private static List<int[]> input;
-   private static List<int[]> output;
-
-   @BeforeClass
-   public static void createArrays()
-   {
-      Random rand = new Random(System.currentTimeMillis());
-
-      input = new ArrayList<int[]>(NUM_ARRAYS);
-      output = new ArrayList<int[]>(NUM_ARRAYS);
-
-      for (int i = 0; i < NUM_ARRAYS; i++)
-      {
-         // find a random length for this array (between 0 [inclusive] and MAX_ARR_LEN [exclusive])
-         // this ensures that our input has length of 0 or more, so we don't test for that
-         int len = rand.nextInt(MAX_ARR_LEN);
-
-         // create this array
-         int[] in = new int[len];
-         // populate this array with random values
-         for (int j = 0; j < in.length; j++)
-         {
-            in[j] = rand.nextInt(); // this ensures that our input is all integers, so we don't test for that
-         }
-         // copy the array that was just created into the array that will eventually be sorted
-         int[] out = Arrays.copyOf(in, in.length);
-         // sort the out array
-         Arrays.sort(out);
-         // store both arrays
-         input.add(i, in);
-         output.add(i, out);
-      }
-   }
-
-   @Test
-   public void test_sameNumInputAndOutputArrays()
-   {
-      assertEquals(input.size(), output.size());
-   }
-
+   private static final int MAX_ARR_LEN = 50000;
+   private static final Random RAND = new Random(System.currentTimeMillis());
 
    @Test
    public void test_inAndOutSameLength()
    {
-      for (int i = 0; i < input.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
-         assertEquals(input.get(i).length, output.get(i).length);
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
+         assertEquals(in.length, out.length);
       }
    }
 
    @Test
    public void test_outputIsProperlySorted()
    {
-      for (int i = 0; i < output.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
-         int[] currArr = output.get(i);
-         for (int j = 0; j < currArr.length - 1; j++)
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
+         for (int j = 0; j < out.length - 1; j++)
          {
-            assertTrue(currArr[j] <= currArr[j + 1]);
+            assertTrue(out[j] <= out[j + 1]);
          }
       }
    }
@@ -85,15 +48,16 @@ public class TestArraySort
    @Test
    public void test_allElementsInInputAccountedFor()
    {
-      for (int i = 0; i < input.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
          // first count input
-         int[] currInArr = input.get(i);
-         Map<Integer, Integer> inputCountMap = countArrayElements(currInArr);
+         Map<Integer, Integer> inputCountMap = countArrayElements(in);
 
          // then count output
-         int[] currOutArr = output.get(i);
-         Map<Integer, Integer> outputCountMap = countArrayElements(currOutArr);
+         Map<Integer, Integer> outputCountMap = countArrayElements(out);
 
          // then compare
          for (Entry<Integer, Integer> entry : inputCountMap.entrySet())
@@ -106,15 +70,16 @@ public class TestArraySort
    @Test
    public void test_noNewElementsInOutput()
    {
-      for (int i = 0; i < input.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
          // first count input
-         int[] currInArr = input.get(i);
-         Map<Integer, Integer> inputCountMap = countArrayElements(currInArr);
+         Map<Integer, Integer> inputCountMap = countArrayElements(in);
 
          // then count output
-         int[] currOutArr = output.get(i);
-         Map<Integer, Integer> outputCountMap = countArrayElements(currOutArr);
+         Map<Integer, Integer> outputCountMap = countArrayElements(out);
 
          // then compare
          for (Entry<Integer, Integer> entry : outputCountMap.entrySet())
@@ -127,32 +92,58 @@ public class TestArraySort
    @Test
    public void test_idempotent()
    {
-      for (int i = 0; i < output.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
-         int[] sorted = output.get(i);
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
          // copy the current output array into the array that will be re-sorted
-         int[] resorted = Arrays.copyOf(sorted, sorted.length);
+         int[] resorted = copy(out);
          // sort the copied array
          Arrays.sort(resorted);
          // compare the two arrays
-         assertArrayEquals(sorted, resorted);
+         assertArrayEquals(out, resorted);
       }
    }
 
    @Test
    public void test_pure()
    {
-      for (int i = 0; i < input.size(); i++)
+      for (int i = 0; i < NUM_ARRAYS; i++)
       {
-         int[] original = input.get(i);
+         int[] in = createArray();
+         int[] out = copy(in);
+         Arrays.sort(out);
          // copy the current input array into the array that will be sorted again
-         int[] sortAgain = Arrays.copyOf(original, original.length);
+         int[] sortAgain = copy(in);
          // sort the copied array
          Arrays.sort(sortAgain);
          // compare the original sorted version of the input array (i.e. the corresponding array in output)
          // and the copied array that was just sorted
-         assertArrayEquals(output.get(i), sortAgain);
+         assertArrayEquals(out, sortAgain);
       }
+   }
+
+   private int[] createArray()
+   {
+      // find a random length for this array (between 0 and MAX_ARR_LEN, inclusive)
+      // this ensures that our input has length of 0 or more, so we don't test for that
+      int len = RAND.nextInt(MAX_ARR_LEN + 1);
+
+      // create this array
+      int[] arr = new int[len];
+      // populate this array with random values
+      for (int j = 0; j < arr.length; j++)
+      {
+         arr[j] = RAND.nextInt(); // this ensures that our input is all integers, so we don't test for that
+      }
+
+      return arr;
+   }
+
+   private int[] copy(int[] arr)
+   {
+      return Arrays.copyOf(arr, arr.length);
    }
 
    private Map<Integer, Integer> countArrayElements(int[] array)
